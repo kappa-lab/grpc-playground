b38a4ac7f5f97c751fd3b62c82f645f3ca13eada
yamagami kenichi <gamiken@kappa-lab.com>
2022-02-09 10:58:36 +0900

    fix package name

--- server/main.go
+++ server/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"flag"
 
-func main() {
-	log.Printf("Hello %s", "world")
+	pb "github.com/kappa-lab/grpc-playground/helloworld"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
 }
