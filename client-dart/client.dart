2a7a394823490c9b9e57f483f767294bb1690285
yamagami kenichi <gamiken@kappa-lab.com>
2022-02-10 17:08:25 +0900

    connected dart client

--- client-dart/client.dart
+++ client-dart/client.dart
@@ -1,5 +1,28 @@
-import 'package:grpc/grpc.dart';
+import 'dart:developer' as dev;
 
-main(List<String> args) {
-  print("dart client");
+import 'package:grpc/grpc.dart';
+import 'lib/src/generated/helloworld.pbgrpc.dart';
+
+main(List<String> args) async {
+  final name = args.isNotEmpty ? args[0] : "world";
+  final age = 20;
+
+  final channel = ClientChannel(
+    "localhost",
+    port: 50052,
+    options: ChannelOptions(credentials: ChannelCredentials.insecure()),
+  );
+  final client = GreeterClient(channel);
+
+  final helloRes = await client.sayHello(HelloRequest(name: name));
+  print("Recieve: $helloRes");
+  print("message: ${helloRes.message}");
+
+  final goodbyRes = await client.sayGoodby(GoodbyRequest(name: name, age: age));
+  print("Recieve: $goodbyRes");
+  print("message: ${goodbyRes.message}");
+
+  await channel.shutdown();
+
+  debugPrint();
 }
